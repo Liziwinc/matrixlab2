60a2d2077eedc8aec267cc48e336a1aa722da76b
Misha Kokorev <mishkokorev35@gmail.com>
2026-03-08 13:58:54 +0300

    4

--- cicd/build_deb.sh
+++ cicd/build_deb.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
+set -e
+
 PACKAGE_NAME="matrix-task"
 VERSION="1.0"
 BUILD_DIR="build_package"
 
+echo "Creating package structure..."
 mkdir -p $BUILD_DIR/DEBIAN
 mkdir -p $BUILD_DIR/usr/bin
 
 cat <<EOF > $BUILD_DIR/DEBIAN/control
-Package: PACKAGE_NAME
-Version: VERSION
+Package: $PACKAGE_NAME
+Version: $VERSION
 Section: utils
 Priority: optional
 Architecture: amd64
-Maintainer: Misha <email@example.com>
-Description: program var 9, lab 2.
+Maintainer: Mikhail
+Description: Matrix processing tool for Lab 2.
 EOF
 
-cd src && make clean && make
-make install DESTDIR=../$BUILD_DIR
-
+echo "Compiling and installing..."
+cd src
+make clean
+make
+cp matrix_task ../$BUILD_DIR/usr/bin/
 cd ..
+
+echo "Building .deb package..."
 dpkg-deb --build $BUILD_DIR
-mv ${$BUILD_DIR}.deb ${PACKAGE_NAME}_${VERSION}.deb
+mv ${BUILD_DIR}.deb ${PACKAGE_NAME}_${VERSION}.deb
+
+echo "Done!"
