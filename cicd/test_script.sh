9bf30ca72385ab0ea80ec6372da27b3c864fedc9
Misha Kokorev <mishkokorev35@gmail.com>
2026-03-29 12:50:34 +0300

    7

--- cicd/test_script.sh
+++ cicd/test_script.sh
@@ -4,19 +4,29 @@ set -e
 echo "Start test"
 cd src && make clean && make
 
-echo "2
-1
-2
-3
--1" > input.txt
+echo "2 1 2 3 -1" > input.txt
 
 ./matrix_task < input.txt > output.txt
 
 if grep -q "4" output.txt && grep -q "\-1" output.txt; then
-  echo "test PASSED: column sum correctly"
-  exit 0
+  echo "test1 PASSED: column sum correctly"
 else
-  echo "test FAILED: column sum not correctly"
+  echo "test1 FAILED: column sum not correctly"
   cat output.txt
   exit 1
 fi
+
+echo "2 2 2 3 -1" > input.txt
+
+./matrix_task < input.txt > output.txt
+
+if grep -q "5" output.txt; then
+  echo "test2 PASSED: sum correctly"
+else
+  echo "test2 FAILED: expected 5"
+  cat output.txt
+  exit 1
+fi
+
+echo "All tests passed"
+exit 0
